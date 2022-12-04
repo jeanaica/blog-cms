3b9e21444e78188593d3c4faac7452e764ba49a7
Jeanaica Suplido <sjeanaica@gmail.com>
2022-12-05 05:04:27 +0800

    ✨ (Modal): Add modal component

---
+++ components/modal/Portal.tsx
@@ -0,0 +1,46 @@
+import { ReactNode, useLayoutEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
+
+type Props = {
+  children: ReactNode;
+  wrapperId: string;
+};
+
+function createWrapperAndAppendToBody(wrapperId: string) {
+  const wrapperElement = document.createElement('div');
+  wrapperElement.setAttribute('id', wrapperId);
+  document.body.appendChild(wrapperElement);
+  return wrapperElement;
+}
+
+function ReactPortal({ children, wrapperId = 'portal-root' }: Props) {
+  const [wrapperElement, setWrapperElement] = useState<HTMLElement | null>(
+    null
+  );
+
+  useLayoutEffect(() => {
+    let element = document.getElementById(wrapperId);
+    let systemCreated = false;
+    // if element is not found with wrapperId or wrapperId is not provided,
+    // create and append to body
+    if (!element) {
+      systemCreated = true;
+      element = createWrapperAndAppendToBody(wrapperId);
+    }
+    setWrapperElement(element);
+
+    return () => {
+      // delete the programatically created element
+      if (systemCreated && element && element.parentNode) {
+        element.parentNode.removeChild(element);
+      }
+    };
+  }, [wrapperId]);
+
+  // wrapperElement state will be null on the very first render.
+  if (wrapperElement === null) return null;
+
+  return createPortal(children, wrapperElement);
+}
+
+export default ReactPortal;
