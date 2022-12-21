94b0c3fc26a2a034d65754f88b5b1b128c4fc97a
Jeanaica Suplido <sjeanaica@gmail.com>
2022-12-22 02:37:40 +0800

    🔍️ (SEO): Add meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,6 +19,35 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
+        <title>CMS Demo</title>
+        <meta
+          property='og:title'
+          content='CMS Demo by Jeanaica'
+        />
+
+        <meta
+          name='image'
+          property='og:image'
+          content='https://demo-cms.jeanaica.com/logo.png'
+        />
+        <meta
+          name='publish_date'
+          property='og:publish_date'
+          content='2022-12-22T00:00:00-0000'
+        />
+        <meta
+          property='og:url'
+          content='https://demo-cms.jeanaica.com'
+        />
+        <meta
+          name='description'
+          property='og:description'
+          content='CMS Demo by Jeanaica using NextJS, ReactJS, TailwindCSS, NodeJS, Typescript. Blog content management system that can add, edit, update blog posts and comments.'
+        />
+        <meta
+          name='author'
+          content='Jeanaica Suplido'
+        />
         <Head></Head>
         <body>
           <Main />
