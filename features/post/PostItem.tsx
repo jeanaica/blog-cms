ae08298eddab8478c17d57204b9b8526d9cf1709
Jeanaica Suplido-Alinsub <sjeanaica@gmail.com>
2023-04-09 04:20:27 +0800

    🚸 (Post): Modify post item on list

--- features/post/PostItem.tsx
+++ features/post/PostItem.tsx
@@ -1,20 +1,22 @@
 import React, { FC } from 'react';
 
 type Props = {
-  id: string;
   title: string;
   content: string;
   updatedAt: number;
 };
 
-const PostItem: FC<Props> = ({ id, title, content, updatedAt }) => {
+const REGEX = /(<([^>]+)>)/gi;
+
+const PostItem: FC<Props> = ({ title, content, updatedAt }) => {
   const formattedDate = new Date(updatedAt).toLocaleString();
+  const strippedContent = content.replace(REGEX, ' ');
 
   return (
     <div className='flex-[4] overflow-hidden flex-col text-left'>
       <h3 className='text-l font-PoppinsSemiBold mt-3 mb-2'>{title}</h3>
       <div className='text-ellipsis overflow-hidden whitespace-nowrap flex-col'>
-        <span className='font-PoppinsExtraLight'>{content}</span>
+        <span className='font-PoppinsExtraLight'>{strippedContent}</span>
       </div>
       <div>
         <span className='font-PoppinsExtraLight text-xs'>{formattedDate}</span>
