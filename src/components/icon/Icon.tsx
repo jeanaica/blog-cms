34b9440ad011f48b476545708fe49d75809223b2
Jeanaica Suplido-Alinsub <sjeanaica@gmail.com>
2026-02-10 12:23:44 +0800

    🐛 Fix icons sizing

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -1,20 +1,29 @@
 import classnames from 'classnames';
 import { FC } from 'react';
 
+const SIZE_CLASSES = {
+  xs: 'text-xs',
+  sm: 'text-sm',
+  base: 'text-base',
+  lg: 'text-lg',
+  xl: 'text-xl',
+  '2xl': 'text-2xl',
+} as const;
+
 type Props = {
   // we want to limit the icons used to material-icons only
   icon: string;
   className?: string;
-  size?: 'sm' | 'base' | 'lg' | 'xl' | '2xl' | number;
+  size?: keyof typeof SIZE_CLASSES | number;
 };
 
-const Icon: FC<Props> = ({ icon, className, size = 'sm' }) => {
+const Icon: FC<Props> = ({ icon, className, size = 'base' }) => {
   return (
     <span
       className={classnames(
         'material-icons-outlined',
         className,
-        typeof size !== 'number' ? `text-${size}` : `text-[${size}px]`
+        typeof size === 'number' ? `text-[${size}px]` : SIZE_CLASSES[size]
       )}>
       {icon}
     </span>
