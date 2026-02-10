15649130ed87c2da80d6451892687cda11bb5382
Jeanaica Suplido-Alinsub <sjeanaica@gmail.com>
2026-02-10 12:24:56 +0800

    ✨ Add content blocks

---
+++ src/components/contentBlock/ContentBlocksEditor.stories.tsx
@@ -0,0 +1,59 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import React from 'react';
+import { useForm, FormProvider } from 'react-hook-form';
+
+import ContentBlocksEditor from './ContentBlocksEditor';
+import { ContentBlock } from './types';
+
+type FormValues = { contentBlocks: ContentBlock[] };
+
+const FormWrapper = ({
+  defaultValues,
+}: {
+  defaultValues?: ContentBlock[];
+}) => {
+  const methods = useForm<FormValues>({
+    defaultValues: { contentBlocks: defaultValues ?? [] },
+  });
+
+  return React.createElement(
+    FormProvider,
+    { ...methods },
+    React.createElement(
+      'form',
+      null,
+      React.createElement(ContentBlocksEditor)
+    )
+  );
+};
+
+const meta = {
+  title: 'Components/ContentBlocksEditor',
+  component: ContentBlocksEditor,
+} satisfies Meta<typeof ContentBlocksEditor>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Empty: Story = {
+  render: () => React.createElement(FormWrapper),
+};
+
+export const WithBlocks: Story = {
+  render: () =>
+    React.createElement(FormWrapper, {
+      defaultValues: [
+        { type: 'text', content: '' },
+        { type: 'gallery', images: [] },
+        { type: 'image' },
+        { type: 'text', content: '' },
+      ],
+    }),
+};
+
+export const SingleBlock: Story = {
+  render: () =>
+    React.createElement(FormWrapper, {
+      defaultValues: [{ type: 'text', content: '' }],
+    }),
+};
